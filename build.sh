107d9948b2c001a749ece46bd59aca1f47f5b8e2
Wenkai Dai <wenkai.dai@ctechm.com>
2025-09-29 11:25:45 +0400

    remove commented-out curl command for master.json in build script

--- build.sh
+++ build.sh
@@ -1,16 +1,19 @@
-#!/bin/sh
-# put on root of the project folder
+##!/bin/sh
+## put on root of the project folder
+#
+#set -e
+#
+#SNIPPET_ID=1
+#BASE_URL="https://gitlab-ultimate.nationalcloud.ae/-/snippets/4/raw/main"
+#
+#mkdir -p maven-build
+#
+#curl -fsSL "${BASE_URL}/mvn_pom_build.py"    -o ./maven-build/mvn_pom_build.py
+##curl -fsSL "${BASE_URL}/master.json"      -o ./maven-build/master.json
+##curl -fsSL "${BASE_URL}/develop.json"    -o ./maven-build/develop.json
+#curl -fsSL "${BASE_URL}/default.json"        -o ./maven-build/default.json
+#
+#python3 ./maven-build/mvn_pom_build.py
 
-set -e
 
-SNIPPET_ID=1
-BASE_URL="https://gitlab-ultimate.nationalcloud.ae/-/snippets/4/raw/main"
-
-mkdir -p maven-build
-
-curl -fsSL "${BASE_URL}/mvn_pom_build.py"    -o ./maven-build/mvn_pom_build.py
-#curl -fsSL "${BASE_URL}/master.json"      -o ./maven-build/master.json
-#curl -fsSL "${BASE_URL}/develop.json"    -o ./maven-build/develop.json
-curl -fsSL "${BASE_URL}/default.json"        -o ./maven-build/default.json
-
-python3 ./maven-build/mvn_pom_build.py
+mvn clean deploy
